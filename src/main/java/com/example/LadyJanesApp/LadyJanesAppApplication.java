2ec5312950989382dd3a7c268f20054c502aeea8
Sarah Goldacker <crossbreedsarah@gmail.com>
2019-05-18 03:49:10 -0500

    initial hello world

---
+++ src/main/java/com/example/LadyJanesApp/LadyJanesAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.LadyJanesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LadyJanesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LadyJanesAppApplication.class, args);
+	}
+
+}
